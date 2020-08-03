29e5077f2c4273cb7efc475d4abbfe2c9717cbc9
Don San Juan Geronimo <don.geronimo@outlook.com>
2020-08-03 03:46:48 -0500

    Utilize URL objects to determine host and path instead of manipulating arrays. Use a private method to find a file when a directory path is given for a Hyperdrive URL (this allows adding files to check in the future in an easy way. Remove extraneous console messages, clarify existing messages, and add some new exceptions to show the errors when the Hyperdrive or Fetch API fail to grab a URL.

--- index.js
+++ index.js
@@ -28,164 +28,127 @@ export class WebmentionValidator {
 
   async checkSource(source, target) {
     let output = false;
-    let sourceRegex = RegExp(target);
-    let sourceSplit = source.split("/");
-    let sourceHost = `${sourceSplit[0]}//${sourceSplit[2]}/`;
-    for (let i = 0; i < 3; i++) { sourceSplit.shift(); }
-    let sourcePath = `/${sourceSplit.join("/")}`;
+    const sourceRegex = RegExp(target);
+    const url = new URL(source);
+    let sourcePath = url.pathname.toString();
 
     // First, try to find the target reference through Beaker
-    try {
-      let sourceHyperdrive = beaker.hyperdrive.drive(sourceHost);
-      let sourceStat = await sourceHyperdrive.stat(sourcePath);
-      // Check the path. If it's a file, continue. If it's a directory, look for 'index.html,' then 'index.md.'
-      if (!sourceStat.isFile()) {
-        let newPath = `${sourcePath}index.html`;
-        try {
-          let newStat = await sourceHyperdrive.stat(newPath);
-          if (newStat.isFile()) { 
-            sourcePath = newPath;
-            sourceStat = newStat;
-          }
-        } catch {}
-      }
-      if (!sourceStat.isFile()) {
-        let newPath = `${sourcePath}index.md`;
-        try {
-          let newStat = await sourceHyperdrive.stat(newPath);
-          if (newStat.isFile()) {
-            sourcePath = newPath;
-            sourceStat = newStat;
-          }
-        } catch {}
-      }
-      if (sourceStat.isFile()) {
-        // Check if the source references the target in metadata
-        console.debug("WebmentionValidator.checkSource: Checking metadata for 'target.'");
-        let metadata = JSON.stringify(sourceStat.metadata);
-        if (sourceRegex.test(metadata)) {
-          console.debug("WebmentionValidator.checkSource: 'target' found in metadata.");
-          output = true;
-        }
-
-        // Check if the source references the target in its HTML
-        if (!output) {
-          let sourceFile = await sourceHyperdrive.readFile(sourcePath, "utf8");
-          if (this.#htmlRegex.test(sourcePath)) {
-            console.debug("WebmentionValidator.checkSource: Is HTML; checking @href/@src for 'target.'");
-            if (this.#checkTargetInSourceHTML(sourceFile, target)) {
-              console.debug("WebmentionValidator.checkSource: 'target' found in HTML.");
-              output = true;
-            }
-          }
-
-          // Check if the source references the target in its contents
-          if (!output) {
-            console.debug("WebmentionValidator.checkSource: Checking content for 'target.'");
-            if (sourceRegex.test(sourceFile)) {
-              console.debug("WebmentionValidator.checkSource: 'target' found in content.");
-              output = true;
-            }
-          }
-        }
-      }
-    
-    // If not through Beaker, use standard Fetch API requests
-    } catch {
+    if (source.startsWith("hyper://")) {
+      let sourceHyperdrive = beaker.hyperdrive.drive(url.origin.toString());
       try {
-        console.debug("WebmentionValidator.checkSource: Using Hyperdrive API failed; using Fetch API.");
+        let sourceStat = await sourceHyperdrive.stat(sourcePath);
+        // Check the path. If it's a file, continue. If it's a directory, look for another file in its place
+        if (!sourceStat.isFile()) {
+          const output = await this.#checkHyperdrivePath(sourcePath, sourceHyperdrive);
+          if (output) {
+            sourcePath = output.path;
+            sourceStat = output.stat;
+          }
+        }
+        if (sourceStat.isFile()) {
+          // Check if the source references the target in metadata
+          let metadata = JSON.stringify(sourceStat.metadata);
+          if (sourceRegex.test(metadata)) {
+            console.debug("WebmentionValidator.checkSource: target URL found in metadata.");
+            output = true;
+          }
+
+          // Check if the source references the target in its HTML
+          if (!output) {
+            let sourceFile = await sourceHyperdrive.readFile(sourcePath, "utf8");
+            if (this.#htmlRegex.test(sourcePath)) {
+              if (this.#checkTargetInSourceHTML(sourceFile, target)) {
+                console.debug("WebmentionValidator.checkSource: target URL found in HTML.");
+                output = true;
+              }
+            }
+
+            // Check if the source references the target in its contents
+            if (!output) {
+              if (sourceRegex.test(sourceFile)) {
+                console.debug("WebmentionValidator.checkSource: target URL found in content.");
+                output = true;
+              }
+            }
+          }
+        } else { throw "Couldn't find a file to check at this URL."; }
+      } catch (error) { console.error("WebmentionValidator.checkSource:", error); }
+    }
+
+    // If not through Beaker, use standard Fetch API requests
+    else {
+      try {
         let response = await fetch(source);
         if (response.ok) {
           // Check if the source references the target in its HTML
           let sourceFile = await response.text();
           let contentType = response.headers.get("content-type");
           if (this.#contentHTML.test(contentType) || this.#contentXHTML.test(contentType)) {
-            console.debug("WebmentionValidator.checkSource: Is HTML; checking @href/@src for 'target.'");
             if (this.#checkTargetInSourceHTML(sourceFile, target)) {
-              console.debug("WebmentionValidator.checkSource: 'target' found in HTML.");
+              console.debug("WebmentionValidator.checkSource: target URL found in HTML.");
               output = true;
             }
           }
 
           // Check if the source references the target in its content
           if (!output) {
-            console.debug("WebmentionValidator.checkSource: Checking content for 'target.'");
             if (sourceRegex.test(sourceFile)) {
-              console.debug("WebmentionValidator.checkSource: 'target' found in content.");
+              console.debug("WebmentionValidator.checkSource: target URL found in content.");
               output = true;
             }
           }
-        }
-      } catch (error) {
-        console.error("WebmentionValidator.checkSource:", error);
-      }
+        } else { throw `Response header returned status ${headers.status} - ${headers.statusText}`; }
+      } catch (error) { console.error("WebmentionValidator.checkSource:", error); }
     }
     return output;
   }
 
   async getTargetEndpoint(target) {
     let output = null;
-    let targetSplit = target.split("/");
-    let targetHost = `${targetSplit[0]}//${targetSplit[2]}/`;
-    for (let i = 0; i < 3; i++) { targetSplit.shift(); }
-    let targetPath = `/${targetSplit.join("/")}`;
+    const url = new URL(target);
+    let targetPath = url.pathname.toString();
     let targetRedirect = null;
 
     // First, try to find @webmention in Beaker
-    try {
-      let targetHyperdrive = beaker.hyperdrive.drive(targetHost);
-      let targetStat = await targetHyperdrive.stat(targetPath);
-      // Check the path. If it's a file, continue. If it's a directory, look for 'index.html,' then 'index.md.'
-      if (!targetStat.isFile()) {
-        let newPath = `${targetPath}index.html`;
-        try {
-          let newStat = await targetHyperdrive.stat(newPath);
-          if (newStat.isFile()) { 
-            targetPath = newPath;
-            targetStat = newStat;
-          }
-        } catch {}
-      }
-      if (!targetStat.isFile()) {
-        let newPath = `${targetPath}index.md`;
-        try {
-          let newStat = await targetHyperdrive.stat(newPath);
-          if (newStat.isFile()) {
-            targetPath = newPath;
-            targetStat = newStat;
-          }
-        } catch {}
-      }
-      if (targetStat.isFile()) {
-        // Check if the metadata mentions this endpoint as @webmention
-        if (targetStat.metadata) {
-          console.debug("WebmentionValidator.getTargetEndpoint: Checking metadata for @webmention.");
-          if (targetStat.metadata.webmention) {
-            console.debug("WebmentionValidator.getTargetEndpoint: @webmention found in metadata.");
-            output = targetStat.metadata.webmention;
+    if (target.startsWith("hyper://")) {
+      let targetHyperdrive = beaker.hyperdrive.drive(url.origin.toString());
+      try {
+        let targetStat = await targetHyperdrive.stat(targetPath);
+        // Check the path. If it's a file, continue. If it's a directory, look for another file in its place
+        if (!targetStat.isFile()) {
+          const output = await this.#checkHyperdrivePath(targetPath, targetHyperdrive);
+          if (output) {
+            targetPath = output.path;
+            targetStat = output.stat;
           }
         }
-
-        // Check if any <a> or <link> tags mention the endpoint with @rel="webmention"
-        if (!output) {
-          let targetFile = await targetHyperdrive.readFile(targetPath, "utf8");
-          if (this.#htmlRegex.test(targetPath)) {
-            console.debug("WebmentionValidator.getTargetEndpoint: Is HTML; checking for @rel=webmention.");
-            let endpointURL = this.#getEndpointInTargetHTML(targetFile);
-            if (endpointURL || (endpointURL === "")) {
-              console.debug("WebmentionValidator.getTargetEndpoint: @webmention found in HTML element with @rel=webmention.");
-              output = endpointURL;
+        if (targetStat.isFile()) {
+          // Check if the metadata mentions this endpoint as @webmention
+          if (targetStat.metadata) {
+            if (targetStat.metadata.webmention) {
+              console.debug("WebmentionValidator.getTargetEndpoint: webmention found in metadata.");
+              output = targetStat.metadata.webmention;
             }
           }
-        }
-      }
+
+          // Check if any <a> or <link> tags mention the endpoint with @rel="webmention"
+          if (!output) {
+            let targetFile = await targetHyperdrive.readFile(targetPath, "utf8");
+            if (this.#htmlRegex.test(targetPath)) {
+              let endpointURL = this.#getEndpointInTargetHTML(targetFile);
+              if (endpointURL || (endpointURL === "")) {
+                console.debug("WebmentionValidator.getTargetEndpoint: webmention found in HTML.");
+                output = endpointURL;
+              }
+            }
+          }
+        } else { throw "Couldn't find a file to check at this URL."; }
+      } catch (error) { console.error("WebmentionValidator.getTargetEndpoint:", error); }
     }
 
     // If not through Beaker, use HTTP Link Headers and Fetch API Requests
-    catch {
+    else {
       try {
-        console.debug("WebmentionValidator.getTargetEndpoint: Using Hyperdrive API failed; using Fetch API.");
-
         // First, check HEAD for an HTTP Link Header
         const headers = await fetch(target, {
           method: "HEAD"
@@ -201,40 +164,34 @@ export class WebmentionValidator {
                 linkParams.forEach(param => {
                   if (!output) {
                     if (this.#relRegex.test(param)) {
-                      console.debug("WebmentionValidator.getTargetEndpoint: 'rel=webmention' found in HTTP Link Headers.");
+                      console.debug("WebmentionValidator.getTargetEndpoint: webmention found in HTTP Headers.");
                       output = url;
-                      console.debug("WebmentionValidator.getTargetEndpoint: output - ", output);
                     }
                   }
                 });
               }
             });
           }
-        }
+        } else { throw `Response header returned status ${headers.status} - ${headers.statusText}`; }
 
         // Then check the HTML
         if (!output) {
-          const response = await fetch(target, {
-            method: "GET"
-          });
+          const response = await fetch(target);
           if (response.ok) {
             // Before anything, check to see if the URL has been redirected
             if (response.redirected) { targetRedirect = response.url; }
             const contentType = response.headers.get("content-type");
             if (this.#contentHTML.test(contentType) || this.#contentXHTML.test(contentType)) {
-              console.debug("WebmentionValidator.getTargetEndpoint: Is (X)HTML; checking for @rel=webmention.");
               const targetFile = await response.text();
               const endpointURL = this.#getEndpointInTargetHTML(targetFile);
               if (endpointURL || (endpointURL === "")) {
-                console.debug("WebmentionValidator.getTargetEndpoint: @webmention found in HTML element with @rel=webmention.");
+                console.debug("WebmentionValidator.getTargetEndpoint: webmention found in HTML.");
                 output = endpointURL;
               }
             }
-          }
+          } else { throw `Response header returned status ${headers.status} - ${headers.statusText}`; }
         }
-      } catch (error) {
-        console.error("WebmentionValidator.getTargetEndpoint:", error);
-      }
+      } catch (error) { console.error("WebmentionValidator.getTargetEndpoint:", error); }
     }
 
     if (output || (output === "")) {
@@ -273,7 +230,35 @@ export class WebmentionValidator {
         }
       }
     });
-    console.log("WebmentionEndpoint.#getEndpointInTargetHTML: output -", output);
+    return output;
+  }
+
+  async #checkHyperdrivePath(path, hyperdrive) {
+    async function checkFile(file) {
+      output = null;
+      try {
+        const stat = await hyperdrive.stat(`${path}${file}`);
+        if (stat.isFile()) {
+          output = {
+            path: `${path}${file}`,
+            stat: stat
+          };
+        }
+      } catch {}
+      return output;
+    }
+
+    let output = null;
+    const files = [
+      "index.html",
+      "index.md"
+    ];
+
+    for (let i = 0; i < files.length; i++) {
+      output = await checkFile(files[i]);
+      if (output) { break; }
+    }
+
     return output;
   }
 }
